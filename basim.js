582428819b7ae0414599f7105d55ec22037353c6
basimkhan <basimkhurram0@gmail.com>
2026-05-25 20:31:15 +0500

    Update basim.js

--- basim.js
+++ basim.js
@@ -25,3 +25,67 @@ function startAddition() {
 let num = prompt("enter your first num")
 let num1 = prompt("enter your second num")
 addnubers(num,num1);
+
+// var remainder = 10 % 3;
+// alert(remainder);
+
+// var num = 5;
+// num--;
+
+// alert(num);
+
+// var num = 4;
+// if (num % 2 == 0){
+//     alert("even")
+// }
+// else{
+//     alert("odd")
+// }
+
+// var user = "admin";
+// var pass = "1234";
+
+// if (user === "admin" && pass === "1234") {
+//     alert("Login Successful");
+// }
+// else {
+//     alert("Wrong Details");
+// }
+
+// var cities = ["Karachi", "Lahore", "Islamabad"];
+
+// alert(cities[0]);
+
+// var fruits = ["Apple", "Banana"];
+
+// fruits.pop("Mango");
+
+// alert(fruits);
+
+// var fruits = ["Apple", "Banana"];
+
+// fruits.splice(0,1, "Mango");
+
+// alert(fruits);
+
+// for (var i = 1; i <= 5; i++) {
+//     alert(i);
+// }
+
+// var matchFound = false;
+// var city = "Karachi";
+
+// if (city === "Karachi") {
+//     matchFound = true;
+// }
+
+// alert(matchFound);
+
+// for (var i = 1; i <= 3; i++) {
+
+//     for (var j = 1; j <= 2; j++) {
+//         alert(i + " " + j);
+//     }
+
+// }
+
